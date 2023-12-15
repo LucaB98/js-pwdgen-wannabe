341b4dafe0ce79eca3233324a7217bed1b72198d
LucaB98 <lucabellan98@gmail.com>
2023-12-15 15:10:04 +0100

    firstname

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log('JS OK')
+
+/*
+1- chiediamo all'utente il suo nome
+2- chiediamo il suo cognome
+3- poi chiediamo il suo colore preferito
+4- Infine generiamo una password con i dati che l'utente ci ha fornito, nella   forma: nomecognomecolorepreferito23
+*/
+
+const firstname = prompt('Come ti chiami?')
+console.log(firstname)
+
